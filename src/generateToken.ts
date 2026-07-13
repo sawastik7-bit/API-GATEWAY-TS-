32336a37e2b95d957ddcb98f4c8075cc747a2398
Sawastik Bhullar <sawastik7@gmail.com>
2026-07-13 18:21:19 +0530

    created a generate token file to generate a jwt to authenticate user

---
+++ src/generateToken.ts
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+
+
+
+dotenv.config();
+if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+}
+
+
+
+type Payload={
+    id:string,
+    name:string,
+     role:"admin" |"user",
+}
+
+const payload:Payload={
+    id:'64abc123',
+    name:"sawastik Bhullar",
+    role:'user'
+}
+
+const token=jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:'1d'});
+
+console.log("your test token : ");
+console.log(token);

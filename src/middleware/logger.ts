1277dec7c172219dec93387ec098c0a89a794e82
Sawastik Bhullar <sawastik7@gmail.com>
2026-07-16 18:21:35 +0530

    complete the logger and created the logger object with generic type logdata in logqueue

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -3,12 +3,18 @@ import type {Request,Response,NextFunction} from 'express';
 
 
 type logData={
-    
-}
+    method:string;
+    url:string;
+    statusCode:number;
+    duration:number;
+    timestamp:string;
+
+};
 
 
 
-const logQueue=new Queue('logQueue',{
+
+const logQueue=new Queue<logData>('logQueue',{
     connection:{
         host:'localhost',
         port:6379
@@ -23,7 +29,27 @@ const logger=(req:Request,res:Response,next:NextFunction)=>{
     res.on('finish',async()=>{
         const duration=Date.now()-start;
 
+        const logData:logData={
+            method:req.method,
+            url:req.originalUrl,
+            statusCode:req.statusCode?? 0,
+            duration,
+            timestamp:new Date().toISOString(),
+        }
+        
+
+try{
+    await logQueue.add('log',logData);
+    console.log(`[Logger] job pushed to queue`);
+}catch(error){
+    console.error(`[Logger] failed to push the job into the queue`);
+}
+
+
 
     })
+    next();
 
 }
+
+export default logger;
